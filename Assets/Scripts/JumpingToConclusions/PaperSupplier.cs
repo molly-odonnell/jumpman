c2ccc8c3844386a2669a4526cbc34ff8fc82c1a3
Charlie Kupets <ckupets@gmail.com>
2019-05-19 17:36:40 -0500

    Added JumpingToConclusionsFiles
    Also added 3 tags.

---
+++ Assets/Scripts/JumpingToConclusions/PaperSupplier.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class PaperSupplier : MonoBehaviour {
+
+	public GameObject thePlane;
+
+	public TextMesh CrimeKey;
+	public TextMesh Desc;
+
+	public TextMesh Name;
+	public TextMesh Alias;
+	public TextMesh Age;
+	public TextMesh Workplace;
+	public TextMesh Height;
+	public TextMesh Sex;
+	public TextMesh Race;
+	public TextMesh Alibi;
+
+	public static List<int> getIndices(string s){
+		var foundIndexes = new List<int>();
+		for (int i = s.IndexOf (' '); i > -1; i = s.IndexOf (' ', i + 1)) {
+			// for loop end when i=-1 ('a' not found)
+			foundIndexes.Add (i);
+		}
+		return foundIndexes;
+	}
+
+	public static string SpliceText(string text, int lineLength) {
+		var foundIndexes = getIndices (text);
+		int lastSpaceIndex = -1;
+		int lastNewlineIndex = -1;
+		int j = 0;
+		while(j < text.Length){
+			if (foundIndexes.Contains (j)) {
+				lastSpaceIndex = j;
+			}
+			if (text [j] == '\n') {
+				lastNewlineIndex = j;
+			}
+			if ((j - lastNewlineIndex > lineLength) && lastSpaceIndex > 0) {
+				text = text.Remove(lastSpaceIndex, 1).Insert (lastSpaceIndex, "\n");
+				return SpliceText (text, lineLength);
+			}
+			j++;
+		}
+		return text;
+	}
+
+	public string reSizeText(string x){
+		float paperWidth = thePlane.GetComponent<MeshRenderer> ().bounds.extents [0];
+		return SpliceText (x, 2 * Mathf.RoundToInt(paperWidth*2));
+	}
+
+	public void autoMove(TextMesh top, TextMesh bottom){
+		float topGuy = top.GetComponent<MeshRenderer> ().bounds.center [1];
+		float topGuy2 = top.GetComponent<MeshRenderer> ().bounds.extents [1];
+		float bottomGuy = bottom.GetComponent<MeshRenderer> ().bounds.center [1];
+		float bottomGuy2 = bottom.GetComponent<MeshRenderer> ().bounds.extents [1];
+		float bottomOfTop = topGuy - topGuy2;
+		float topOfBottom = bottomGuy + bottomGuy2;
+		bottom.transform.Translate (new Vector3 (0, bottomOfTop - topOfBottom));
+	}
+
+	public void loadSuspectProfile (string[,] input, int caseNum, int suspectNum) {
+		int Row = 2 + caseNum * 4 + suspectNum;
+		Name.text = SpliceText (input [2, Row], 13);
+		Age.text = SpliceText (input[4,Row], 12);
+		//autoMove (Name, Age);
+		Alias.text = SpliceText ("        " + input[3,Row], 16);
+		//autoMove (Age, Alias);
+		Workplace.text = SpliceText (input[5,Row], 20);
+		//autoMove (Alias, Workplace);
+		Height.text = SpliceText (input[6,Row], 10);
+		//autoMove (Workplace, Height);
+		Sex.text = SpliceText (input[7,Row], 7);
+		//autoMove (Workplace, Sex);
+		Race.text = SpliceText (input[8,Row], 13);
+		//autoMove (Sex, Race);
+		Alibi.text = SpliceText ("         " + input[9,Row], 33);
+		//autoMove (Race, Alibi);
+	}
+
+	public void loadCrimeDesc (string[,] input, int caseNum) {
+		int Row = 2 + caseNum * 4;
+		CrimeKey.text = reSizeText ("Crime: " + input[0,Row]);
+		Desc.text = reSizeText ("Description: " + input[1,Row]);
+		autoMove (CrimeKey, Desc);
+	}
+}
---
+++ Assets/Scripts/JumpingToConclusions/ReadDemo.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ReadDemo : MonoBehaviour {
+
+	public TextAsset csv; 
+
+	public GameObject paperContainer;
+	public GameObject suspectProfilePrefab;
+	public GameObject CrimeDescPrefab;
+	public GameObject privacyScreen;
+	public AudioSource pageFlip;
+
+	private int recentInput = 0;
+	private bool waitingForInput = false;
+
+	private string[,] outputGrid;
+	private int NumberOfCases;
+	private int i;
+
+	void Start () {
+		CSVReader.DebugOutputGrid( CSVReader.SplitCsvGrid(csv.text) ); 
+		outputGrid = CSVReader.SplitCsvGrid (csv.text);
+		NumberOfCases = int.Parse(outputGrid[1,0]);
+
+		i = 0;
+		summonPapers ();
+		waitingForInput = true;
+
+	}
+
+	void Update () {
+		if(waitingForInput){
+			if(Input.GetKeyDown(KeyCode.RightArrow)){
+				recentInput = 3;
+			}
+			if(Input.GetKeyDown(KeyCode.LeftArrow)){
+				recentInput = 1;
+			}
+			if(Input.GetKeyDown(KeyCode.DownArrow)){
+				recentInput = 2;
+			}
+			if(recentInput != 0){
+				waitingForInput = false;
+				StartCoroutine (BlackScreen());
+			}
+		}
+	}
+
+	void summonPapers (){
+		GameObject myCPaper = Instantiate (CrimeDescPrefab);
+		myCPaper.GetComponent<PaperSupplier>().loadCrimeDesc(outputGrid, i);
+		myCPaper.transform.position = new Vector3 (18, 20, 0);
+		myCPaper.transform.SetParent (paperContainer.transform);
+		for (int j = 1; j<=3; j++) {
+			GameObject myPaper = Instantiate (suspectProfilePrefab);
+			myPaper.GetComponent<PaperSupplier>().loadSuspectProfile(outputGrid, i, j);
+			myPaper.transform.position = new Vector3 ((j-1)*18, 0, 0);
+			myPaper.transform.SetParent (paperContainer.transform);
+			myPaper.tag = j.ToString ();
+		}
+		if (i == NumberOfCases - 1) {
+			i = 0;
+		} else {
+			i++;
+		}
+		waitingForInput = true;
+	}
+
+	IEnumerator BlackScreen()
+	{
+		pageFlip.Play (1000);
+
+		foreach (Transform child in paperContainer.transform) {
+			if (!(child.tag == recentInput.ToString ())) {
+				GameObject.Destroy (child.gameObject);
+			}
+		}
+
+		yield return new WaitForSeconds (0.25f);
+
+		foreach (Transform child in paperContainer.transform) {
+			GameObject.Destroy (child.gameObject);
+		}
+
+		Renderer rend = privacyScreen.GetComponent<Renderer>();
+		rend.enabled = true;
+
+		yield return new WaitForSeconds (0.25f);
+		rend.enabled = false;
+		recentInput = 0;
+		summonPapers();
+	}
+
+}
